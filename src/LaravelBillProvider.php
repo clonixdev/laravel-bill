9153cc2474edaf463fedf0bcbe056f0b76e08eae
CodigoWWW <git@codigowww.com>
2021-06-24 23:51:41 -0300

    Migrations

--- src/LaravelBillProvider.php
+++ src/LaravelBillProvider.php
@@ -8,8 +8,24 @@ class LaravelBillProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
+      
+        if ($this->app->runningInConsole()) {
+            // Export the migration
+            if (! class_exists('CreateInvoicesTable')) {
+              $this->publishes([
+                __DIR__ . '/../database/migrations/2021_04_30_094108_create_invoices_table.php' => database_path('migrations/2021_04_30_094108_create_invoices_table.php'),
+                __DIR__ . '/../database/migrations/2021_04_30_094130_create_invoice_items_table.php' => database_path('migrations/2021_04_30_094130_create_invoice_items_table.php'),
+                __DIR__ . '/../database/migrations/2021_04_30_094154_create_pay_methods_table.php' => database_path('migrations/2021_04_30_094154_create_pay_methods_table.php'),
+                __DIR__ . '/../database/migrations/2021_04_30_094155_create_pay_method_records_table.php' => database_path('migrations/2021_04_30_094155_create_pay_method_records_table.php'),
+                __DIR__ . '/../database/migrations/2021_04_30_094156_create_currencies_table.php' => database_path('migrations/2021_04_30_094156_create_currencies_table.php'),
+                // you can add any number of migrations here
+              ], 'migrations');    
+              
+              $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('bill.php'),
+              ], 'config');
+            }
+          }
         $this->registerRoutes();
 
     }
