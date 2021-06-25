d11d7974d20ce2c5241c49cbab8de0a5e4cf3f04
CodigoWWW <git@codigowww.com>
2021-06-24 22:35:42 -0300

    delete routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,19 +8,19 @@ use Clonixdev\LaravelBill\Http\Controllers\PayMethodController;
 Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoices.index');
 Route::get('/invoices/{id}', [InvoiceController::class, 'show'])->name('invoices.show');
 Route::post('/invoices/{id}/checkout', [InvoiceController::class, 'checkout'])->name('invoices.checkout');
-
+Route::delete('/invoices/{id}', [InvoiceController::class, 'destroy'])->name('invoices.destroy');
 
 Route::get('/currencies', [CurrencyController::class, 'index'])->name('currencies.index');
 Route::get('/currencies/{id}', [CurrencyController::class, 'show'])->name('currencies.show');
 Route::post('/currencies', [CurrencyController::class, 'store'])->name('currencies.store');
 Route::put('/currencies/{id}', [CurrencyController::class, 'update'])->name('currencies.update');
-
+Route::delete('/currencies/{id}', [CurrencyController::class, 'destroy'])->name('currencies.destroy');
 
 Route::get('/pay-methods', [PayMethodController::class, 'index'])->name('pay-method.index');
 Route::get('/pay-methods/{id}', [PayMethodController::class, 'show'])->name('pay-method.show');
 Route::post('/pay-methods', [PayMethodController::class, 'store'])->name('pay-method.store');
 Route::put('/pay-methods/{id}', [PayMethodController::class, 'update'])->name('pay-method.update');
-
+Route::delete('/pay-methods/{id}', [PayMethodController::class, 'destroy'])->name('pay-method.destroy');
 
 
 Route::get('/external-link', [PayMethodController::class, 'externalLink'])->name('pay-method.external-link');
